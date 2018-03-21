f8f6451e5c9f4b47d71a5bf85aa1cc55cd51e6e1
guangxiao <maguangxiao@foxmail.com>
2018-03-21 21:10:55 +0800

    init the project

---
+++ GXInputLimitTextField/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GXInputLimitTextField
+//
+//  Created by GuangXiao on 2018/3/21.
+//  Copyright © 2018年 guangxiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
